c2207746bb959aeb7d7fd660d91537d20138a186
Supern0vaTJ <Tjha846@gmail.com>
2024-09-12 18:15:11 +0530

    removed apostrophe

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
     <div className="text-3xl text-black-500">
-      <h3>Welcome to the AI based ATS friendly CV generator. Just bring your Job Description. Rest we'll handle
+      <h3>Welcome to the AI based ATS friendly CV generator. Just bring your Job Description. Rest we will handle
       </h3>
     </div>
     <Button onClick={()=>(window.location.href = '/dashboard') }>Dashboard</Button>
