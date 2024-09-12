53677f8c6b8f868a8c60bf70bd0aab20c31956ee
Supern0vaTJ <Tjha846@gmail.com>
2024-09-12 17:16:35 +0530

    1st push

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -5,8 +5,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-    <div className="text-6xl text-green-500">Unprotected for everyone to see
-    
+    <div className="text-3xl text-black-500">
+      <h3>Welcome to the AI based ATS friendly CV generator. Just bring your Job Description. Rest we'll handle
+      </h3>
     </div>
     <Button onClick={()=>(window.location.href = '/dashboard') }>Dashboard</Button>
     </>
